70473ff0d80b0a6e4b1e91fcc3640e54595673db
Christina Cook <cmc.cook7@gmail.com>
2020-12-01 21:45:41 -0700

    Pass tests to initialize a Deck with an array of cards and count the cards

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,29 +6,30 @@ const Card = require('../src/Card');
 
 describe('Deck', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const deck = new Deck();
 
     expect(Deck).to.be.a('function');
   });
 
-  it.skip('should be an instance of Deck', function() {
+  it('should be an instance of Deck', function() {
     const deck = new Deck();
 
     expect(deck).to.be.an.instanceof(Deck);
   });
 
-  it.skip('should be instantiated with an array of Card objects', function() {
+  it('should be instantiated with an array of Card objects', function() {
     const card1 = new Card(1, `What is Forrest\'s favorite toy?`, ['benebone', 'tennis ball', 'stuffed squirrel'], 'ball');
     const card2 = new Card(2, `What is Forrest\'s favorite activity?`, ['hiking', 'playing fetch', 'swimming'], 'playing fetch');
     const card3 = new Card(3, `Forrest lives in a/an...`, ['apartment', 'house', 'condo'], 'condo');
+    const card4 = new Card(4, `Forrest lives in a/an...`, ['apartment', 'house', 'condo'], 'condo');
 
-    const deck = new Deck([card1, card2, card3]);
+    const deck = new Deck([card1, card2, card3, card4]);
 
-    expect(deck).to.equal([card1, card2, card3]);
+    expect(deck.cards).to.deep.equal([card1, card2, card3, card4]);
   });
 
-  it.skip('should know how many cards are in the Deck', function() {
+  it('should know how many cards are in the Deck', function() {
     const card1 = new Card(1, `What is Forrest\'s favorite toy?`, ['benebone', 'tennis ball', 'stuffed squirrel'], 'ball');
     const card2 = new Card(2, `What kind of animal is Forrest?`, ['cat', 'dog', 'turtle'], 'dog');
     const card3 = new Card(3, `What is Forrest\'s favorite activity?`, ['hiking', 'playing fetch', 'swimming'], 'playing fetch');
@@ -38,7 +39,6 @@ describe('Deck', function() {
 
     deck.countCards();
 
-    expect(deck).to.equal([card1, card2, card3, card4]);
-    expect(deck).to.have.lengthOf(4);
-  })
+    expect(deck.cards).to.have.lengthOf(4);
+  });
 })
