8aebd12dfe64e43f5dd6747d33b18e60261c6e3a
Christina Cook <cmc.cook7@gmail.com>
2020-12-01 22:06:22 -0700

    Create files and pseudocode for Round class

---
+++ test/Round-test.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Round', function() {
+
+  //it should be a function
+
+  //it should be an instance of Round
+
+  //it should include a method to return the current
+  //card being played
+  //the current card should be the first card in the deck (the array of cards)
+  //round.returnCurrentCard();
+
+  //it should include a method that updates the turns count
+  //needs a property to keep track of (increment) turn count
+  //the turns count increments regardless of whether the answer was correct
+  //the next card then becomes the current card
+  //guesses are evaluated
+  //incorrect guesses are stored (via the id) in an array
+  //of incorrectGuesses
+  //feedback is returned
+
+  //it should include a method to calculate and return percentage
+  //of correct guesses
+
+  //it should include a method to print a message when the game is over
+  
+})
