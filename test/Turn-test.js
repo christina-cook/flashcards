02a1d91d23a96545d6b9655df0bf4e94671f75b5
Christina Cook <cmc.cook7@gmail.com>
2020-12-01 19:21:37 -0700

    Pass first set of Turn tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,28 +2,29 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const turn = new Turn();
 
     expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', function() {
+  it('should be an instance of Turn', function() {
     const turn = new Turn();
 
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it.skip('should be instantiated with a string representing the user\'s guess', function() {
+  it('should be instantiated with a string representing the user\'s guess', function() {
     const turn = new Turn('ball');
 
     expect(turn.guess).to.equal('ball');
   })
 
-  it.skip('should be instantiated with a card object', function() {
+  it('should be instantiated with a card object', function() {
     const card = new Card(1, `What is Forrest\'s favorite toy?`, ['benebone', 'tennis ball', 'stuffed squirrel'], 'ball');
     const turn = new Turn('ball', card);
 
