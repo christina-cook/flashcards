0df99582b0eda8bbeb087b8adb760bf4fdac9783
Christina Cook <cmc.cook7@gmail.com>
2020-12-02 22:09:06 -0700

    Add dec and round variables to beforeEach

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,50 +7,38 @@ const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 describe('Round', function() {
-  let card1, card2, card3
+  let card1, card2, card3, deck, round;
 
   beforeEach(function() {
     card1 = new Card(1, `What is Forrest\'s favorite toy?`, ['benebone', 'tennis ball', 'stuffed squirrel'], 'ball');
     card2 = new Card(2, `What kind of animal is Forrest?`, ['cat', 'dog', 'turtle'], 'dog');
     card3 = new Card(3, `What is Forrest\'s favorite activity?`, ['hiking', 'playing fetch', 'swimming'], 'playing fetch');
-    // const deck = new Deck([card1, card2, card3]);
-    // const round = new Round(deck);
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
   });
 
-  it('should be a function', function() {
-    const round = new Round();
 
+  it('should be a function', function() {
     expect(Round).to.be.a('function');
   });
 
 
   it('should be an instance of Round', function() {
-    const round = new Round();
-
     expect(round).to.be.an.instanceof(Round);
   });
 
 
   it('should include a deck with an array of cards', function() {
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     expect(round.deck.cards).to.deep.equal([card1, card2, card3])
   });
 
 
   it('should indicate the number of turns, starting at 0', function() {
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     expect(round.turns).to.equal(0);
   });
 
 
   it('should update the number of turns each time a turn is taken', function() {
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     round.takeTurn('benebone');
 
     expect(round.turns).to.equal(1);
@@ -63,9 +51,6 @@ describe('Round', function() {
 
 
   it('should return a new card at the end of each turn', function() {
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-
     round.returnCurrentCard();
 
     expect(round.returnCurrentCard()).to.equal(card1);
