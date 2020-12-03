47413a60e7a57e873d5942aaf46bbd4674250a29
Christina Cook <cmc.cook7@gmail.com>
2020-12-03 15:39:35 -0700

    Set up start method in Game class

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,28 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+  }
+
+  start() {
+    // create Cards
+    const cards = prototypeQuestions.map(data => {
+      return card = new Card(data.id, data.question, data.answers, data.correctAnswer)
+    });
+    // put Cards in a Deck
+    let deck = new Deck(cards);
+    // create a Round using the Deck
+    this.currentRound = new Round(deck);
+    this.printMessage();
+    this.printQuestion(this.currentRound);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
---
+++ test/Game-test.js
@@ -0,0 +1,10 @@
+// should keep track of the current round (property) ***
+
+// should include a method to start the game ***
+// 
+// creates Cards
+// puts Cards in a Deck
+// creates a new Round using the Deck
+
+// invokes printMessage to display message in the CLI
+// invokes printQuestion
