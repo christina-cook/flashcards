fbec94350c6284bf685a7e4fe5cfd0e7d1d6e852
Christina Cook <cmc.cook7@gmail.com>
2020-12-03 17:51:39 -0700

    Change to arrow functions in Game test

--- test/Game-test.js
+++ test/Game-test.js
@@ -5,22 +5,22 @@ const Round = require('../src/Round');
 const Game = require('../src/Game');
 
 
-describe('Game', function() {
+describe('Game', () => {
   let game;
 
-  beforeEach(function() {
+  beforeEach(() => {
     game = new Game();
   });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Game).to.be.a('function');
   });
 
-  it('should be an instance of Game', function() {
+  it('should be an instance of Game', () => {
     expect(game).to.be.an.instanceof(Game);
   });
 
-  it('should instantiate a new round', function() {
+  it('should instantiate a new round', () => {
     game.start();
     expect(game.currentRound).to.be.an.instanceof(Round);
   });
